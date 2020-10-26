4f09cb6bd583129ef4ac43dfca6f1934fa6aee71
Kassandra Torres <ktorres1393@gmail.com>
2020-10-25 21:45:19 -0400

    adds functionality to buttons

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
-console.log('jQuery: ', $)
+// IPO - input process output 
+
+//Constants and Variables
+let skills;
+
+// Cached element references
+const $button = $('button');
+const $ul = $('ul');
+const $input = $('input')
+
+//Event listeners
+$button.on('click', handleAddSkill);
+$ul.on('click', handleDelete);
+
+//Functions
+init();
+
+function init () {
+    skills = [];
+    render();
+}
+
+function handleAddSkill(){
+    // store the value from the imput tag inside a local variable
+    const skill = $input.val()
+    //check to make sure we have skill data;
+    if(skill) {
+        //create the UI for a skill
+        const $skill = $(`<li><span>X</span>${skill}</li>`)
+        //push skill UI into the skills list
+        skills.push($skill);
+        //clear the input tag's value
+        $input.val('');
+        //render()
+        render()
+    } else return;
+    
+    
+    
+     
+}
+
+function handleDelete (){
+    alert('Elements inside of ul tag clicked')
+}
+
+function render(){
+    //to take the list of skills in the skills array and add them to the ul tag
+    if(!skills.length) $ul.css('margin-bottom', '30px');
+    else $ul.css('margin-bottom', '0px')
+    $ul.html(skills);
+}
+
+
