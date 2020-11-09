5e1b92095e78aa9f56dae15a7cd8bd71514a801e
Kassandra Torres <ktorres1393@gmail.com>
2020-11-09 15:32:41 -0500

    adds delete functionality

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const $button = $('button');
 const $ul = $('ul');
 const $input = $('input')
 
+
 //Event listeners
 $button.on('click', handleAddSkill);
 $ul.on('click', handleDelete);
@@ -15,18 +16,18 @@ $ul.on('click', handleDelete);
 //Functions
 init();
 
-function init () {
+function init() {
     skills = [];
     render();
 }
 
-function handleAddSkill(){
+function handleAddSkill() {
     // store the value from the imput tag inside a local variable
     const skill = $input.val()
     //check to make sure we have skill data;
-    if(skill) {
+    if (skill) {
         //create the UI for a skill
-        const $skill = $(`<li><span>X</span>${skill}</li>`)
+        const $skill = $(`<li class='remove'><span>X</span>${skill}</li>`)
         //push skill UI into the skills list
         skills.push($skill);
         //clear the input tag's value
@@ -34,21 +35,26 @@ function handleAddSkill(){
         //render()
         render()
     } else return;
-    
-    
-    
-     
+
+//1.47.56
 }
 
-function handleDelete (){
-    alert('Elements inside of ul tag clicked')
-}
+function handleDelete() {
+    $ul.on('click', '.remove', function() {
+        $(this).closest('li').remove();
+        
+          
+      });
+        
 
-function render(){
+ }
+   
+
+
+
+function render() {
     //to take the list of skills in the skills array and add them to the ul tag
-    if(!skills.length) $ul.css('margin-bottom', '30px');
+    if (!skills.length) $ul.css('margin-bottom', '30px');
     else $ul.css('margin-bottom', '0px')
     $ul.html(skills);
 }
-
-
